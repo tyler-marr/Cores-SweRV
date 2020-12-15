cd1a64ddb8775f602201a6fb0fefba4845c1ce9d
Brandon Cuellar <bc109@rune.cms.waikato.ac.nz>
2020-12-16 10:54:13 +1300

    Added uart

--- testbench/decoder.sv
+++ testbench/decoder.sv
@@ -56,11 +56,11 @@ axi_slv  lmem(
     .rst_l(rst_l),
     .arvalid(w_arvalid),
     .arready(w_arready),
-    .araddr(araddr),
-    .arid(arid),
-    .arlen(arlen),
-    .arburst(arburst),
-    .arsize(arsize),
+    .araddr(w_araddr),
+    .arid(w_arid),
+    .arlen(w_arlen),
+    .arburst(w_arburst),
+    .arsize(w_arsize),
 
     .rvalid(w_rvalid),
     .rready(w_rready),
@@ -71,7 +71,7 @@ axi_slv  lmem(
 
     .awvalid(w_awvalid),
     .awready(w_awready),
-    .awaddr(awaddr),
+    .awaddr(w_awaddr),
     .awid(w_awid),
     .awlen(w_awlen),
     .awburst(w_awburst),
@@ -125,19 +125,89 @@ star_m star(
     .bid(x_bid)
 );
 
-parameter   MAILBOX_ADDR = 32'hD0580000;
-parameter   BOUNDARY = 32'h80000000;
+// axi_slv  uart(
+//     .aclk(aclk),
+//     .rst_l(rst_l),
+//     .arvalid(u_arvalid),
+//     .arready(u_arready),
+//     .araddr(u_araddr),
+//     .arid(u_arid),
+//     .arlen(u_arlen),
+//     .arburst(u_arburst),
+//     .arsize(u_arsize),
+
+//     .rvalid(u_rvalid),
+//     .rready(u_rready),
+//     .rdata(u_rdata),
+//     .rresp(u_rresp),
+//     .rid(u_rid),
+//     .rlast(u_rlast),
+
+//     .awvalid(u_awvalid),
+//     .awready(u_awready),
+//     .awaddr(u_awaddr),
+//     .awid(u_awid),
+//     .awlen(u_awlen),
+//     .awburst(u_awburst),
+//     .awsize(u_awsize),
+
+//     .wdata(u_wdata),
+//     .wstrb(u_wstrb),
+//     .wvalid(u_wvalid),
+//     .wready(u_wready),
+
+//     .bvalid(u_bvalid),
+//     .bready(u_bready),
+//     .bresp(u_bresp),
+//     .bid(u_bid)
+// );
+
+
+uart_dpi 
+    #(
+        .UART_DPI_ADDR_WIDTH(32),
+        // .tcp_port( 5678),
+        // .port_name("UART DPI"),
+        // .welcome_message("Welcome to the UART DPI simulated serial interface.\n\r"),
+        // .character_timeout_clk_count(100),
+        // parameter listen_on_local_addr_only = 1,
+        // parameter receive_buffer_size  = (100 * 1024),
+        // parameter transmit_buffer_size = (100 * 1024),
+
+        // parameter print_informational_messages = 1,
+        // TRACE_DATA = 0
+    )
+    uart_port
+    ( 
+        .wb_clk_i(aclk),
+        .wb_rst_i(~rst_l), // There is no need to assert reset at the beginning.
+
+        .wb_adr_i(u_awaddr), //[UART_DPI_ADDR_WIDTH-1:0] 
+        .wb_dat_i(u_wdata[31:0]), //[UART_DPI_DATA_WIDTH-1:0]
+        .wb_dat_o(u_rdata[31:0]), //[UART_DPI_DATA_WIDTH-1:0]
+
+        .wb_we_i(u_awvalid),
+        .wb_stb_i(1),
+        .wb_cyc_i(1),
+        .wb_ack_o(wb_ack_o),
+        .wb_err_o(wb_err_o),
+        .wb_sel_i(1), //[3:0]
+
+        .int_o(int_o)  // UART interrupt request
+    );
+
+wire int_o;
+wire wb_ack_o;
+wire wb_err_o;
+
+parameter   MAILBOX_ADDR = 32'hd0580000;
+parameter   BOUNDARY1 = 32'h80000000;
+parameter   BOUNDARY2 = 32'hF8000000;
 wire [63:0] WriteData;
 wire        mailbox_write;
-reg  [31:0] oaraddr;
 
-always @(aclk) begin
-    if (araddr != MAILBOX_ADDR)
-        oaraddr =  araddr;
-end
-
-assign mailbox_write = (oaraddr <  BOUNDARY) ? lmem.mailbox_write : star.mailbox_write;
-assign WriteData     = (oaraddr <  BOUNDARY) ? lmem.WriteData     : star.WriteData;
+assign mailbox_write = awvalid && awaddr==MAILBOX_ADDR && rst_l;
+assign WriteData = wdata;
 
 initial begin
     $readmemh("program.hex",  lmem.mem);
@@ -213,71 +283,128 @@ wire            x_bready;
 wire [1:0]      x_bresp;
 wire [3:0]      x_bid;
 
+// uart
+wire            u_arvalid;
+wire            u_arready;
+wire [31:0]     u_araddr;
+wire [TAGW-1:0] u_arid;
+wire [7:0]      u_arlen;
+wire [1:0]      u_arburst;
+wire [2:0]      u_arsize;
+
+wire            u_rvalid;
+wire            u_rready;
+wire [63:0]     u_rdata;
+wire [1:0]      u_rresp;
+wire [TAGW-1:0] u_rid;
+wire            u_rlast;
+
+wire            u_awvalid;
+wire            u_awready;
+wire [31:0]     u_awaddr;
+wire [TAGW-1:0] u_awid;
+wire [7:0]      u_awlen;
+wire [1:0]      u_awburst;
+wire [2:0]      u_awsize;
+
+wire            u_wvalid;
+wire            u_wready;
+wire [63:0]     u_wdata;
+wire [7:0]      u_wstrb;
+
+wire            u_bvalid;
+wire            u_bready;
+wire [1:0]      u_bresp;
+wire [3:0]      u_bid;
 
 // lmem
-assign w_arvalid = (oaraddr <  BOUNDARY) ? arvalid : 1'b0;
-assign w_araddr  = (oaraddr <  BOUNDARY) ? araddr  : 32'b0;
-assign w_arid    = (oaraddr <  BOUNDARY) ? arid    : 4'b0;
-assign w_arlen   = (oaraddr <  BOUNDARY) ? arlen   : 8'b0;
-assign w_arburst = (oaraddr <  BOUNDARY) ? arburst : 2'b0;
-assign w_arsize  = (oaraddr <  BOUNDARY) ? arsize  : 3'b0;
+assign w_arvalid = (araddr <  BOUNDARY1) ? arvalid : 1'b0;
+assign w_araddr  = (araddr <  BOUNDARY1) ? araddr  : 32'b0;
+assign w_arid    = (araddr <  BOUNDARY1) ? arid    : 4'b0;
+assign w_arlen   = (araddr <  BOUNDARY1) ? arlen   : 8'b0;
+assign w_arburst = (araddr <  BOUNDARY1) ? arburst : 2'b0;
+assign w_arsize  = (araddr <  BOUNDARY1) ? arsize  : 3'b0;
 
-assign w_rready  = (oaraddr <  BOUNDARY) ? rready  : 1'b0;
+assign w_rready  = (araddr <  BOUNDARY1) ? rready  : 1'b0;
 
-assign w_awvalid = (oaraddr <  BOUNDARY) ? awvalid : 1'b0;
-assign w_awaddr  = (oaraddr <  BOUNDARY) ? awaddr  : 32'b0;
-assign w_awid    = (oaraddr <  BOUNDARY) ? awid    : 4'b0;
-assign w_awlen   = (oaraddr <  BOUNDARY) ? awlen   : 8'b0;
-assign w_awburst = (oaraddr <  BOUNDARY) ? awburst : 2'b0;
-assign w_awsize  = (oaraddr <  BOUNDARY) ? awsize  : 3'b0;
+assign w_awvalid = (araddr <  BOUNDARY1) ? awvalid : 1'b0;
+assign w_awaddr  = (araddr <  BOUNDARY1) ? awaddr  : 32'b0;
+assign w_awid    = (araddr <  BOUNDARY1) ? awid    : 4'b0;
+assign w_awlen   = (araddr <  BOUNDARY1) ? awlen   : 8'b0;
+assign w_awburst = (araddr <  BOUNDARY1) ? awburst : 2'b0;
+assign w_awsize  = (araddr <  BOUNDARY1) ? awsize  : 3'b0;
 
-assign w_wvalid  = (oaraddr <  BOUNDARY) ? wvalid  : 1'b0;
-assign w_wdata   = (oaraddr <  BOUNDARY) ? wdata   : 64'b0;
-assign w_wstrb   = (oaraddr <  BOUNDARY) ? wstrb   : 8'b0;
+assign w_wvalid  = (araddr <  BOUNDARY1) ? wvalid  : 1'b0;
+assign w_wdata   = (araddr <  BOUNDARY1) ? wdata   : 64'b0;
+assign w_wstrb   = (araddr <  BOUNDARY1) ? wstrb   : 8'b0;
 
-assign w_bready  = (oaraddr <  BOUNDARY) ? bready  : 1'b0;
+assign w_bready  = (araddr <  BOUNDARY1) ? bready  : 1'b0;
 
 
 // star
-assign x_arvalid = (oaraddr >= BOUNDARY) ? arvalid : 1'b0;
-assign x_araddr  = (oaraddr >= BOUNDARY) ? araddr  : 32'b0;
-assign x_arid    = (oaraddr >= BOUNDARY) ? arid    : 4'b0;
-assign x_arlen   = (oaraddr >= BOUNDARY) ? arlen   : 8'b0;
-assign x_arburst = (oaraddr >= BOUNDARY) ? arburst : 2'b0;
-assign x_arsize  = (oaraddr >= BOUNDARY) ? arsize  : 3'b0;
+assign x_arvalid = (araddr >= BOUNDARY1 && araddr < BOUNDARY2) ? arvalid : 1'b0;
+assign x_araddr  = (araddr >= BOUNDARY1 && araddr < BOUNDARY2) ? araddr  : 32'b0;
+assign x_arid    = (araddr >= BOUNDARY1 && araddr < BOUNDARY2) ? arid    : 4'b0;
+assign x_arlen   = (araddr >= BOUNDARY1 && araddr < BOUNDARY2) ? arlen   : 8'b0;
+assign x_arburst = (araddr >= BOUNDARY1 && araddr < BOUNDARY2) ? arburst : 2'b0;
+assign x_arsize  = (araddr >= BOUNDARY1 && araddr < BOUNDARY2) ? arsize  : 3'b0;
 
-assign x_rready  = (oaraddr >= BOUNDARY) ? rready  : 1'b0;
+assign x_rready  = (araddr >= BOUNDARY1 && araddr < BOUNDARY2) ? rready  : 1'b0;
 
-assign x_awvalid = (oaraddr >= BOUNDARY) ? awvalid : 1'b0;
-assign x_awaddr  = (oaraddr >= BOUNDARY) ? awaddr  : 32'b0;
-assign x_awid    = (oaraddr >= BOUNDARY) ? awid    : 4'b0;
-assign x_awlen   = (oaraddr >= BOUNDARY) ? awlen   : 8'b0;
-assign x_awburst = (oaraddr >= BOUNDARY) ? awburst : 2'b0;
-assign x_awsize  = (oaraddr >= BOUNDARY) ? awsize  : 3'b0;
+assign x_awvalid = (araddr >= BOUNDARY1 && araddr < BOUNDARY2) ? awvalid : 1'b0;
+assign x_awaddr  = (araddr >= BOUNDARY1 && araddr < BOUNDARY2) ? awaddr  : 32'b0;
+assign x_awid    = (araddr >= BOUNDARY1 && araddr < BOUNDARY2) ? awid    : 4'b0;
+assign x_awlen   = (araddr >= BOUNDARY1 && araddr < BOUNDARY2) ? awlen   : 8'b0;
+assign x_awburst = (araddr >= BOUNDARY1 && araddr < BOUNDARY2) ? awburst : 2'b0;
+assign x_awsize  = (araddr >= BOUNDARY1 && araddr < BOUNDARY2) ? awsize  : 3'b0;
 
-assign x_wvalid  = (oaraddr >= BOUNDARY) ? wvalid  : 1'b0;
-assign x_wdata   = (oaraddr >= BOUNDARY) ? wdata   : 64'b0;
-assign x_wstrb   = (oaraddr >= BOUNDARY) ? wstrb   : 8'b0;
+assign x_wvalid  = (araddr >= BOUNDARY1 && araddr < BOUNDARY2) ? wvalid  : 1'b0;
+assign x_wdata   = (araddr >= BOUNDARY1 && araddr < BOUNDARY2) ? wdata   : 64'b0;
+assign x_wstrb   = (araddr >= BOUNDARY1 && araddr < BOUNDARY2) ? wstrb   : 8'b0;
 
-assign x_bready  = (oaraddr >= BOUNDARY) ? bready  : 1'b0;
+assign x_bready  = (araddr >= BOUNDARY1 && araddr < BOUNDARY2) ? bready  : 1'b0;
+
+
+// uart
+assign u_arvalid = (araddr >= BOUNDARY2) ? arvalid : 1'b0;
+assign u_araddr  = (araddr >= BOUNDARY2) ? araddr  : 32'b0;
+assign u_arid    = (araddr >= BOUNDARY2) ? arid    : 4'b0;
+assign u_arlen   = (araddr >= BOUNDARY2) ? arlen   : 8'b0;
+assign u_arburst = (araddr >= BOUNDARY2) ? arburst : 2'b0;
+assign u_arsize  = (araddr >= BOUNDARY2) ? arsize  : 3'b0;
+
+assign u_rready  = (araddr >= BOUNDARY2) ? rready  : 1'b0;
+
+assign u_awvalid = (araddr >= BOUNDARY2) ? awvalid : 1'b0;
+assign u_awaddr  = (araddr >= BOUNDARY2) ? {28'h0, awaddr[3:0]}  : 32'b0;
+assign u_awid    = (araddr >= BOUNDARY2) ? awid    : 4'b0;
+assign u_awlen   = (araddr >= BOUNDARY2) ? awlen   : 8'b0;
+assign u_awburst = (araddr >= BOUNDARY2) ? awburst : 2'b0;
+assign u_awsize  = (araddr >= BOUNDARY2) ? awsize  : 3'b0;
+
+assign u_wvalid  = (araddr >= BOUNDARY2) ? wvalid  : 1'b0;
+assign u_wdata   = (araddr >= BOUNDARY2) ? wdata   : 64'b0;
+assign u_wstrb   = (araddr >= BOUNDARY2) ? wstrb   : 8'b0;
+
+assign u_bready  = (araddr >= BOUNDARY2) ? bready  : 1'b0;
 
 
 // Slave Channels
-assign arready   = (oaraddr <  BOUNDARY) ? w_arready : x_arready;
+assign arready   = (araddr <  BOUNDARY1) ? w_arready : (araddr < BOUNDARY2) ? x_arready  : u_arready;
 
-assign rvalid    = (oaraddr <  BOUNDARY) ? w_rvalid  : x_rvalid;
-assign rdata     = (oaraddr <  BOUNDARY) ? w_rdata   : x_rdata;
-assign rresp     = (oaraddr <  BOUNDARY) ? w_rresp   : x_rresp;
-assign rid       = (oaraddr <  BOUNDARY) ? w_rid     : x_rid;
-assign rlast     = (oaraddr <  BOUNDARY) ? w_rlast   : x_rlast;
+assign rvalid    = (araddr <  BOUNDARY1) ? w_rvalid  : (araddr < BOUNDARY2) ? x_rvalid   : u_rvalid;
+assign rdata     = (araddr <  BOUNDARY1) ? w_rdata   : (araddr < BOUNDARY2) ? x_rdata    : u_rdata;
+assign rresp     = (araddr <  BOUNDARY1) ? w_rresp   : (araddr < BOUNDARY2) ? x_rresp    : u_rresp;
+assign rid       = (araddr <  BOUNDARY1) ? w_rid     : (araddr < BOUNDARY2) ? x_rid      : u_rid;
+assign rlast     = (araddr <  BOUNDARY1) ? w_rlast   : (araddr < BOUNDARY2) ? x_rlast    : u_rlast;
 
-assign awready   = (oaraddr <  BOUNDARY) ? w_awready : x_awready;
+assign awready   = (araddr <  BOUNDARY1) ? w_awready : (araddr < BOUNDARY2) ? x_awready  : u_awready;
 
-assign wready    = (oaraddr <  BOUNDARY) ? w_wready  : x_wready;
+assign wready    = (araddr <  BOUNDARY1) ? w_wready  : (araddr < BOUNDARY2) ? x_wready   : u_wready;
 
-assign bvalid    = (oaraddr <  BOUNDARY) ? w_bvalid  : x_bvalid;
-assign bresp     = (oaraddr <  BOUNDARY) ? w_bresp   : x_bresp;
-assign bid       = (oaraddr <  BOUNDARY) ? w_bid     : x_bid;
+assign bvalid    = (araddr <  BOUNDARY1) ? w_bvalid  : (araddr < BOUNDARY2) ? x_bvalid   : u_bvalid;
+assign bresp     = (araddr <  BOUNDARY1) ? w_bresp   : (araddr < BOUNDARY2) ? x_bresp    : u_bresp;
+assign bid       = (araddr <  BOUNDARY1) ? w_bid     : (araddr < BOUNDARY2) ? x_bid      : u_bid;
 
 endmodule
 `endif
--- testbench/star.sv
+++ testbench/star.sv
@@ -78,8 +78,16 @@ always @ ( posedge aclk or negedge rst_l) begin
 end
 
 always @ ( negedge aclk) begin
-    if(arvalid) memdata <= {mem[araddr+7], mem[araddr+6], mem[araddr+5], mem[araddr+4],
-                            mem[araddr+3], mem[araddr+2], mem[araddr+1], mem[araddr]};
+    if(arvalid) memdata <= {
+        mem[araddr+7]+1'h1, 
+        mem[araddr+6]+1'h1, 
+        mem[araddr+5]+1'h1, 
+        mem[araddr+4]+1'h1,
+        mem[araddr+3]+1'h1, 
+        mem[araddr+2]+1'h1, 
+        mem[araddr+1]+1'h1, 
+        mem[araddr+0]+1'h1};
+        
     if(awvalid) begin
         if(wstrb[7]) mem[awaddr+7] = wdata[63:56];
         if(wstrb[6]) mem[awaddr+6] = wdata[55:48];
